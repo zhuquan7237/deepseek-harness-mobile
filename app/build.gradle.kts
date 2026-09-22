061a0c31a2399ed2842af0015c61a43365aa75d5
Quan Zhu <zhuquan7237@qq.com>
2026-09-22 21:28:12 +0800

    release v0.2.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dsh.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.2.22"
+        versionCode = 27
+        versionName = "0.2.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
