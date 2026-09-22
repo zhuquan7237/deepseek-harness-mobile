6c7f6ab3ed7baf4e7ef34798daa1f43492795b87
zhuquan7237 <zhuquan7237@users.noreply.github.com>
2026-09-22 18:51:14 +0800

    fix: import height (the divider needs it)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dsh.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.2.12"
+        versionCode = 16
+        versionName = "0.2.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
