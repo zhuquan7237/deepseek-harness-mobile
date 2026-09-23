617e432e2c13b09c1ddc222e2532e211884212a3
Quan Zhu <zhuquan7237@qq.com>
2026-09-23 13:43:24 +0800

    release v0.2.34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dsh.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "0.2.33"
+        versionCode = 38
+        versionName = "0.2.34"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
