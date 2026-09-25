98a01f8916e54f14732d0901480ee3c9ccd44077
Quan Zhu <zhuquan7237@qq.com>
2026-09-25 22:52:46 +0800

    release v0.2.72

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dsh.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 79
-        versionName = "0.2.71"
+        versionCode = 80
+        versionName = "0.2.72"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
