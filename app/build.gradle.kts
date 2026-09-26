0941ac938f9609bf577bd869442d552f8001635d
Quan Zhu <zhuquan7237@qq.com>
2026-09-26 20:37:34 +0800

    release v0.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dsh.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 92
-        versionName = "0.3.6"
+        versionCode = 93
+        versionName = "0.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
